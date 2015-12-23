f9186250cc43aa62d6ffa2cc2ac6db2d1393bbd6
Alprensia <tonite32@gmail.com>
2015-12-23 18:40:44 +0900

    imp가 url패턴을 들고 있고 collector는 url을 던지면 http 또는 local file을 가져오도록 역할 수정.

--- src/collector.js
+++ src/collector.js
@@ -5,29 +5,10 @@ var collector = {};
 
 (function()
 {
-	//콜렉터는 컴포넌트 이름과 실제 url이 맵핑되어야 한다.
-	//그런데 보통 패턴으로 처리할듯하다.
-	//pattern에 name을 replace한다.
-	
-	//그리고 로컬파일인경우와 remote파일 혹은 request url인 경우가 있겠지.
-	
-	this.pattern = "";
-		
-	//컴포넌트 이름을 받아서 실제 string을 돌려주는 착한아이.
-	//컴포넌트 이름과 실제 url의 매핑을 가지고 있어야 할것이다?
-	//config 파일을 통해 입력을 받을 수 있게끔 합시다.
-	//1:1맵핑이나, 패턴맵핑.
-	
-	this.getComponent = function(name, callback)
+	this.getComponent = function(url, callback)
 	{
 		try
 		{
-			if(this.pattern)
-			{
-				
-			}
-			var url = this.pattern.replace(/{{name}}/gi, name);
-			
 			//가져온다.
 			if(url.match(/^https?:\/\//gi))
 			{
@@ -40,12 +21,10 @@ var collector = {};
 				{
 				    if(error)
 				    {
-				    	that.parent.logger.error(error);
 				    	callback(error);
 				    }
 				    else
 				    {
-				    	that.parent.logger.log(response.statusCode, body);
 				        callback(null, body);
 				    }
 				});
--- src/imp.js
+++ src/imp.js
@@ -1,216 +1,111 @@
-//메인모듈
-
 var imp = {};
 
 (function()
 {
-	this.logger = console;
+	this.collector = require("./collector");
 	
-	this.collector = require(_path.src + "/collector");
-	this.collector.parent = this;
+	this.pattern = "";
 	
-	this.map = {}; // 레이아웃 이름과 실제 url을 매핑한다. 레이아웃은 화면의 기본틀이 되는 index파일. 여러개가 될수도 있음.
-//	this.map['/'] = "index"; // 1:1맵핑
-//	this.map['/[a-z]*'] = "index"; // n:1맵핑
-//	this.map['*'] = "index"; // n:1맵핑
-//	this.map['/([a-z]*)/([0-9]*)'] = "(1)_(2)"; // n:n맵핑
+	this.vars = {}; //고정치환변수
 	
-	this.vars = {}; //치환변수
-//	this.vars = {test : "테스트"}; // #{test}
-//	this.vars = {test : {abc : "에이비씨"}}; // #{test.abc}
-	
-	this.fixedUrlParam = {};
-	
-	this.getPage = function(url, callback)
+	this.getHtml = function(componentName, params, callback)
 	{
 		try
 		{
-			this.parseUrl(url);
+			var that = this;
 			
-			var layoutName = "";
-			for(var key in this.map)
+			var url = this.pattern.replace(/{{name}}/gi, componentName);
+			this.collector.getComponent(url, function(err, data)
 			{
-				var regx = new RegExp(key, "gi");
-				var result = regx.exec(url);
-				if(result)
+				if(err)
 				{
-					layoutName = this.map[key];
-					if(result.length > 2)
-					{
-						for(var i=1; i<result.length; i++)
-						{
-							if(result[i].trim())
-							{
-								layoutName = layoutName.replace("(" + i + ")", result[i].trim());
-							}
-						}
-					}
-					
-					break;
+					callback(err);
+					return;
 				}
-			}
-			
-			if(layoutName)
-			{
-				this.collector.getComponent(layoutName, function(err, data)
+				
+				data = this.replaceVars(this.extend({}, this.vars, params), data);
+				
+				//데이터 바인드를 했다고 치고
+				var matchList = data.match(/\$\{[a-zA-Z0-9\_\-\(\):\/]*\}/gi);
+				if(matchList)
 				{
-					if(err)
+					var forEach = require('async-foreach').forEach;
+					forEach(matchList, function(match, index)
 					{
-						this.logger.error(err);
-						return;
-					}
-
-					data = this.replaceVars(data);
-					
-					//데이터 바인드를 했다고 치고
-					
-					this.parseComponent(data, function(resultData)
-					{
-						callback(resultData);
+						var done = this.async();
+						var name = match.replace("${", "").replace("}", "");
+						that.getHtml(name, params, function(err, html)
+						{
+							if(err)
+							{
+								data = data.replace(match, "<!-- no such component, " + match + " -->");
+							}
+							else if(html)
+							{
+								//여기서 component에 대한 데이터바인딩을 해야한다.
+								data = data.replace(match, html);
+							}
+							
+							done();
+						});
+					},
+					function(){
+						callback(null, data);
 					});
-					
-				}.bind(this));
-			}
+				}
+				else
+				{
+					//컴포넌트가 없으면
+					callback(null, data);
+				}
+				
+			}.bind(this));
 		}
 		catch(err)
 		{
-			this.logger.error(err.stack);
-		}
-		finally
-		{
-			return null;
+			callback(err);
 		}
 	};
-	
-	this.parseUrl = function(url)
-	{
-		url = url.replace(/^https?:\/\//gi, ""); //앞에 처리하고. //해시는 어떻게 하지?
-		
-		var split = url.split("?");
-		
-		// 고정 url 파라미터와 쿼리스트링 분리
-		url = url[0];
-		var qs = split[1];
-		
-		var split = url.split("/");
-		for(var i=0; i<split.length; i++)
-		{
-			if(i == split.length-1 && split[i].indexOf("#") != -1)
-				split[i] = split[i].split("#")[0];
-			
-			this.fixedUrlParam[i] = split[i];
-		}
-		
-		this.vars.query = {};
-		
-		if(qs)
-		{
-			split = qs.split("&");
-			for(var i=0; i<split.length; i++)
-			{
-				var keyValue = split[i].split("=");
-				if(keyValue[1] && keyValue[1].indexOf("#") != -1)
-					keyValue[1] = keyValue[1].split("#")[0];
 
-				this.vars.query[keyValue[0]] = decodeURIComponent(keyValue[1]);
-			}
-		}
-	};
-	
-	this.replaceVars = function(data)
+	this.replaceVars = function(vars, data)
 	{
-		for(var key in this.vars)
+		var matchList = data.match(/#{[a-zA-Z0-9\.]*}/gi);
+		if(matchList)
 		{
-			var matchList = data.match(/#{[a-zA-Z0-9\.]*}/gi);
-			if(matchList)
+			for(var i=0; i<matchList.length; i++)
 			{
-				for(var i=0; i<matchList.length; i++)
+				var value = null;
+				
+				var key = matchList[i].replace("#{", "").replace("}", "");
+				if(key.indexOf("\.") != -1)
 				{
-					var value = this.vars[key];
-					if(matchList[i].indexOf("\.") != -1)
+					value = vars;
+					var split = key.split("\.");
+					for(var j=0; j<split.length; j++)
 					{
-						var split = matchList[i].split("\.");
-						
-						for(var j=1; j<split.length; j++)
+						if(value.hasOwnProperty(split[j]))
 						{
-							value = value[split[j].replace("}", "")];
+							value = value[split[j]];
+						}
+						else
+						{
+							value = null;
+							break;
 						}
 					}
-					
-					data = data.replace(matchList[i], value);
 				}
+				else
+				{
+					if(vars.hasOwnProperty(key))
+						value = vars[key];
+				}
+				
+				if(value)
+					data = data.replace(matchList[i], value);
 			}
 		}
 		
 		return data;
-	};
-	
-	this.parseComponent = function(data, callback)
-	{
-		var that = this;
-		
-		var matchList = data.match(/\$\{[a-zA-Z0-9\_\-\(\):\/]*\}/gi);
-		if(matchList)
-		{
-			var forEach = require('async-foreach').forEach;
-			forEach(matchList, function(match, index)
-			{
-				try
-				{
-					var done = this.async();
-					
-					var name = match.replace("${", "").replace("}", "");
-					
-					var nameMatchList = name.match(/(\([0-9a-zA-Z-_:\/]*\))/gi);
-					if(nameMatchList)
-					{
-						for(var j=0; j<nameMatchList.length; j++)
-						{
-							var value = nameMatchList[j].replace("(", "").replace(")", "");
-							var split = value.split(":");
-							var index = split[0];
-							var defaultValue = split[1];
-							
-							if(that.fixedUrlParam[index])
-								value = that.fixedUrlParam[index];
-							else if(defaultValue)
-								value = defaultValue;
-							else
-								value = "";
-
-							name = name.replace(nameMatchList[j], value);
-						}
-					}
-					
-					that.collector.getComponent(name, function(err, component)
-					{
-						if(err)
-						{
-							done();
-							return;
-						}
-						
-						//여기서 component에 대한 데이터바인딩을 해야한다.
-						
-						data = data.replace(match, component);
-						
-						done();
-					});
-				}
-				catch(err)
-				{
-					that.logger.error(err.stack);
-				}
-			},
-			function(){
-				callback(data);
-			});
-		}
-	};
-	
-	this.setLayoutMap = function(map)
-	{
-		this.map = map;
 	};
 	
 	this.setVars = function(vars)
@@ -218,15 +113,21 @@ var imp = {};
 		this.vars = vars;
 	};
 	
-	this.setComponentPattern = function(pattern)
+	this.setPattern = function(pattern)
 	{
-		this.collector.setPattern(pattern);
+		this.pattern = pattern;
 	};
 	
-	this.setLogger = function(logger)
+	this.extend = function(target)
 	{
-		this.logger = logger;
-	};
+	    var sources = [].slice.call(arguments, 1);
+	    sources.forEach(function (source) {
+	        for (var prop in source) {
+	            target[prop] = source[prop];
+	        }
+	    });
+	    return target;
+	}
 	
 }).call(imp);
 
